a3eef98254b5343aa6d79142372c0cb4b88f3879
yalcinHaratoglu <yalcinharat@gmail.com>
2024-02-16 12:26:30 +0300

    .env added

---
+++ src/useMovies.jsx
@@ -0,0 +1,54 @@
+import { useEffect, useState } from "react";
+
+const API_KEY = import.meta.env.VITE_API_KEY;
+
+export function useMovies(query) {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    async function fetchMovies() {
+      try {
+        setIsLoading(true);
+        setError("");
+        const res = await fetch(
+          `http://www.omdbapi.com/?apikey=${API_KEY}&s=${query}`,
+          { signal: controller.signal }
+        );
+
+        if (!res.ok) {
+          throw new Error("Something went wrong!");
+        }
+        const data = await res.json();
+        if (data.Response === "False") {
+          throw new Error("Movie NOT FOUND!");
+        }
+
+        setMovies(data.Search);
+        setError("");
+      } catch (err) {
+        if (err.name !== "AbortError") {
+          console.log(err.message);
+          setError(err.message);
+        }
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    if (query.length < 3) {
+      setMovies([]);
+      setError("");
+      return;
+    }
+
+    fetchMovies();
+    return function () {
+      controller.abort();
+    };
+  }, [query]);
+
+  return { movies, isLoading, error };
+}
